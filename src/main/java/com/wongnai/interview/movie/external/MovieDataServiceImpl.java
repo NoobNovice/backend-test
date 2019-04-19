3fc737d1d90d58fd78237893e96f369cbfbf87a4
NoobNovice <30790608+NoobNovice@users.noreply.github.com>
2019-04-19 21:20:04 +0700

    implement fetchAll

--- src/main/java/com/wongnai/interview/movie/external/MovieDataServiceImpl.java
+++ src/main/java/com/wongnai/interview/movie/external/MovieDataServiceImpl.java
@@ -1,10 +1,16 @@
 package com.wongnai.interview.movie.external;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestOperations;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 @Component
 public class MovieDataServiceImpl implements MovieDataService {
@@ -23,7 +29,19 @@ public class MovieDataServiceImpl implements MovieDataService {
 		// Step 1 => Implement this method to download data from MOVIE_DATA_URL and fix any error you may found.
 		// Please noted that you must only read data remotely and only from given source,
 		// do not download and use local file or put the file anywhere else.
-
-		return null;
+		MoviesResponse data = new MoviesResponse();
+		try {
+			URL url = new URL(MOVIE_DATA_URL);
+			data = objectMapper.readValue(url, MoviesResponse.class);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (JsonParseException e) {
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return data;
 	}
 }
